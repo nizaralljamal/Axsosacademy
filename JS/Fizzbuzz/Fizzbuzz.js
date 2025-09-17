7c9bea6845f074d2948c6ddb813104d74ef15a42
nizaralljamal <nizaralljamal@gmail.com>
2025-09-17 08:37:07 +0300

    Add Fizzbuzz files

---
+++ JS/Fizzbuzz/Fizzbuzz.js
@@ -0,0 +1,55 @@
+for(var i = 1; i <= 100 ;i++) {
+ if(i%3==0 && i%5==0) {
+ console.log("FizzBuzz");
+ 
+ }
+ else if(i%5==0) {
+ console.log("Buzz");
+ 
+ }
+ 
+ else if ( i%3==0){
+ console.log("Fizz");
+ 
+ }
+ 
+ else {
+ console.log(i);
+ 
+ }
+}
+ 
+for(var i=1;i<=20;i++){
+ if(i%2!==0) {
+ console.log(i);
+ 
+ }
+}
+ 
+for (var i=100;i>=0;i--) {
+ if (i%3==0){
+ console.log(i);
+ 
+ 
+ }
+ 
+}
+ 
+for(var i=4;i>=-3.5;i-=1.5){
+ console.log(i);
+ 
+}
+var sum=0
+for(var i=1;i<=100;i++){
+ sum =sum+i
+}
+ 
+console.log(sum);
+ 
+var fac=1
+for(var i=1;i<=12;i++){
+ fac =fac*i
+}
+ 
+console.log(fac);
+ 
